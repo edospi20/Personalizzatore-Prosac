b98d784333a1c7a5515c641296f09c9560ae4220
Edoardo Spigarelli <edospi20@hotmail.it>
2015-04-24 16:51:50 +0200

    created repository

---
+++ Jquery-circular-path/animate-circle.js
@@ -0,0 +1,20 @@
+
+var init = function() {
+	var images = [];
+	for (var k = 0; k < 11; ++k)
+		images.push('color' + k + '.png');
+
+	var clothsCircle = new $.RailwayCloth($('#wrapper'), images, config.railway);
+
+	$('#left').click(function() {
+		$(this).prop('disabled', true);
+		clothsCircle.fastForward($(this));
+	});
+	$('#right').click(function() {
+		$(this).prop('disabled', true);
+		clothsCircle.rewind($(this));
+	});
+	
+	$('#left').prop('disabled', false);
+	$('#right').prop('disabled', false);
+};
