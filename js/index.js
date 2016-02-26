824ee99c3ac53a05b8a716f23f9fb518414a39c2
Davide Laezza <truzzialrogo@gmx.com>
2016-02-26 20:17:50 +0100

    Used div instead of ul as slick wrapper

--- js/index.js
+++ js/index.js
@@ -12,10 +12,16 @@ cloths.slick({
 	infinite: true,
 	initialSlide: 0,
 	lazyLoad: 'progressive',
-	slide: 'li',
+	slide: 'div',
 	slidesToShow: 3,
 	slidesToScroll: 3,
-	useCSS: true
+	useCSS: true,
+	
+	// TODO: LE FRECCE NON FUNZIANOH
+	
+	prevArrow: '<button type="button" class="slick-prev">&lt;</button>',
+	nextArrow: '<button type="button" class="slick-prev">&gt;</button>',
+	
 });
 
 });
