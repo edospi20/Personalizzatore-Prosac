c87c5ad786ddce901f74c1a85f18e74b784bb68e
Edoardo Spigarelli <edospi20@hotmail.it>
2016-03-02 16:44:52 +0100

    realizzati i 3 slider

--- js/index.js
+++ js/index.js
@@ -1,31 +1,30 @@
 (function($) {
 
-$(function() {
+	$(function() {
 
-var slider = $('.slider');
-var cloths = slider.find('div:eq(1) > div');
-var buttons = slider.find('button');
-
-cloths.slick({
-	accessibility: true,
-	arrows: true,
-	dots: true,
-	focusOnSelect: true,
-	infinite: true,
-	initialSlide: 0,
-	lazyLoad: 'progressive',
-	slide: 'div',
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	useCSS: true,
-	
-	// TODO: LE FRECCE NON FUNZIANOH
-	
-	prevArrow: buttons.eq(0),
-	nextArrow: buttons.eq(1)
-	
-});
-
-});
-
+		$('.slider').each(function() {
+			var $this = $(this);	//Identifica il singolo elemento che è dentro slider
+			var slider = $this.find('div:nth-of-type(2) > div');
+			var buttons = $this.find('button');
+			
+			slider.slick({
+				accessibility: true,
+				arrows: true,
+				dots: true,
+				focusOnSelect: true,
+				infinite: true,
+				initialSlide: 0,
+				lazyLoad: 'progressive',
+				slide: 'div',
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				useCSS: true,
+				
+				// TODO: LE FRECCE NON FUNZIANOH
+				
+				prevArrow: buttons.eq(0),
+				nextArrow: buttons.eq(1)	
+			});		
+		});
+	});
 })(jQuery);
